464d140aaa5d1258202152d82f3c5ca84ea46714
DamirLut <lutfrahmanovdamir3@gmail.com>
2026-07-10 19:42:36 +0500

    feat: add HttpError and formatRubles to utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,23 @@
+export class HttpError extends Error {
+	readonly statusCode: number;
+
+	constructor(statusCode: number, message: string) {
+		super(message);
+		this.name = "HttpError";
+		this.statusCode = statusCode;
+	}
+}
+
+export function formatRubles(amount: number, currency = "RUB", locale = "ru-RU"): string {
+	const formatter = new Intl.NumberFormat(locale, {
+		style: "currency",
+		currency,
+		maximumFractionDigits: 2,
+		minimumFractionDigits: 0,
+	});
+	return formatter.format(amount);
+}
+
 export function createLogger(prefix: string, color = Color.white) {
 	return (...args: unknown[]) => console.log(`[${color}${prefix}${Color.white}]`, ...args);
 }
